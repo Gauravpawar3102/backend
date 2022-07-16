474ccbca347da0d561d54d41eacb205494a9ee26
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-07-16 23:24:23 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const format = require('date-format');
-
 const app = express();
+
+// swagger docs related
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 4000;
 
